8a318e7ed07b5ddd8fc670481421ffa05bb973fc
hrw2013 <harrisonrobertwoods@gmail.com>
2025-11-12 21:31:44 -0500

    Add firebase configuration file

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyA10rt9j5WUTp8-T0i7qcpWp1uIu3oI_Wc",
+  authDomain: "login-dashboard-4aa87.firebaseapp.com",
+  projectId: "login-dashboard-4aa87",
+  storageBucket: "login-dashboard-4aa87.firebasestorage.app",
+  messagingSenderId: "196276515757",
+  appId: "1:196276515757:web:c8420101386483f40de724",
+  measurementId: "G-LBEHKCTYLF"
+};
